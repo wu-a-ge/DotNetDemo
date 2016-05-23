e7a1f52ff9757339e8f975d3592c5477c5e9d644
xiaofu <wfygowxf1@163.com>
2016-05-24 00:03:55 +0800

    demo

---
+++ vs2010.consoleApp.demo/vs2010.consoleApp.demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vs2010.consoleApp.demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.HelloWorld service = new localhost.HelloWorld();
+            service.RequestSOAPHeader = new RequestSOAPHeader();
+            service.RequestSOAPHeader.user = "user";
+            service.RequestSOAPHeader.password = "abcd";
+            service.sayHi("abc");
+        }
+    }
+}
---
+++ vs2010.consoleApp.demo/vs2010.consoleApp.demo/RequestSOAPHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vs2010.consoleApp.demo
+{
+    public class RequestSOAPHeader : System.Web.Services.Protocols.SoapHeader
+    {
+        public string user { get; set; }
+        public string password { get; set; }
+    }
+}
