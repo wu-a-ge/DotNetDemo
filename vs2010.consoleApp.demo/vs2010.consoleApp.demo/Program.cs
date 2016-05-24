2dabc360ca97e2b0b130c5193dd9970097310d72
xiaofu <wfygowxf1@163.com>
2016-05-24 14:21:22 +0800

    demo

--- vs2010.consoleApp.demo/vs2010.consoleApp.demo/Program.cs
+++ vs2010.consoleApp.demo/vs2010.consoleApp.demo/Program.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using vs2010.consoleApp.demo.localhost;
 namespace vs2010.consoleApp.demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            localhost.HelloWorld service = new localhost.HelloWorld();
-            service.RequestSOAPHeader = new RequestSOAPHeader();
-            service.RequestSOAPHeader.user = "user";
-            service.RequestSOAPHeader.password = "abcd";
-            service.sayHi("abc");
+           ReaderInfoService service=new ReaderInfoService();
+            service.RequestSOAPHeader=new RequestSOAPHeader();
+            service.RequestSOAPHeader.user = "root";
+            service.RequestSOAPHeader.password="root";
+            //service.getListsByIds("A1234", "is_active");
+            service.getUserInfo("abc", "1234");
         }
     }
 }
